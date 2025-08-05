386a7325eddd4a2c2ec506370d6bfc21bf1ed8e8
AlexisLeor <109035202+AlexisLeor@users.noreply.github.com>
2025-08-05 02:30:00 +0200

    Update scraper_manager.py

--- scraper_manager.py
+++ scraper_manager.py
@@ -1,10 +1,30 @@
 from scrapers.cnap import scrape_cnap
 from scrapers.cipac import scrape_cipac
-# from scrapers.autres import scrape_autres # Ajouter les autres scrapers ici
+from scrapers.villa_kujoyama import scrape_villa_kujoyama
+from scrapers.villa_medicis import scrape_villa_medicis
+from scrapers.villa_albertine import scrape_villa_albertine
+from scrapers.pro_helvetia import scrape_pro_helvetia
+from scrapers.transartists import scrape_transartists
+from scrapers.ceaac import scrape_ceaac
+from scrapers.goethe_institut import scrape_goethe_institut
+from scrapers.institut_francais import scrape_institut_francais
+from scrapers.villa_velazquez import scrape_villa_velazquez
+from scrapers.villa_swagatam import scrape_villa_swagatam
+from scrapers.villa_ndar import scrape_villa_ndar
 
-def collect_all_opencalls():
+def run_all_scrapers():
     all_calls = []
-    all_calls += scrape_cnap()
-    all_calls += scrape_cipac()
-    # all_calls += scrape_autres()
+    all_calls.extend(scrape_cnap())
+    all_calls.extend(scrape_cipac())
+    all_calls.extend(scrape_villa_kujoyama())
+    all_calls.extend(scrape_villa_medicis())
+    all_calls.extend(scrape_villa_albertine())
+    all_calls.extend(scrape_pro_helvetia())
+    all_calls.extend(scrape_transartists())
+    all_calls.extend(scrape_ceaac())
+    all_calls.extend(scrape_goethe_institut())
+    all_calls.extend(scrape_institut_francais())
+    all_calls.extend(scrape_villa_velazquez())
+    all_calls.extend(scrape_villa_swagatam())
+    all_calls.extend(scrape_villa_ndar())
     return all_calls
